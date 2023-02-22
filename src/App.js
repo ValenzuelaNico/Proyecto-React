1529329454fad16e520646bcaf29d3bb81745182
nicolas <valenzuela-nicolas@hotmail.com>
2023-02-21 22:20:33 -0300

    Martes

--- src/App.js
+++ src/App.js
@@ -2,26 +2,45 @@ import './App.css';
 import reySi from './reyes-img/rey_sisebuto.png'
 import reyLeo from './reyes-img/rey_leogivildo.png'
 import reyAta from './reyes-img/rey_atanagildo.png'
+import incognito from './reyes-img/rey_incognito.png'
 
 function App() {
-  let nombres = ["Sisebuto","Leogivildo","Atanagildo"]
+  let nombres = ["Sisebuto","Leogivildo","Atanagildo","Visto"]
+  
+  const cambiaFoto = (e)=>{
+    if(e.target.src.includes("incognito")) {
+      e.target.style.visibility="hidden"
+    }else
+{    e.target.src = incognito;
+     e.target.parentNode.style.backgroundColor = "white"
+}
+  }
+  const cambiaNombre =(e)=>{
+    if (e.target.innerHTML == nombres[3]){
+      e.target.innerHTML= ' ';
+    }else
+    {
+      e.target.innerHTML = nombres[3]
+    }
+    
+    
+  }
   return (
     <div className='container'>
       <div className='rey-img'>
-        <img src={reySi} className="img"></img>
-        <h4>{nombres[0]}</h4>
+        <img src={reySi} className="img" onClick={cambiaFoto}></img>
+        <h4 onClick={cambiaNombre}>{nombres[0]}</h4>
       </div>
       <div className='rey-img'>
-        <img src={reyLeo} className="img"></img>
-        <h4>Leogivildo</h4>
+        <img src={reyLeo} className="img" onClick={cambiaFoto}></img>
+        <h4 onClick={cambiaNombre}>{nombres[1]}</h4>
       </div>
       <div className='rey-img'>
-        <img src={reyAta}className="img"></img>
-        <h4>Atanagildo</h4>
+        <img src={reyAta}className="img" onClick={cambiaFoto}></img>
+        <h4 onClick={cambiaNombre}>{nombres[2]}</h4>
       </div>
     </div>
   );
 }
 
 export default App;
-// 3 cajas cada una con imagen cada una
